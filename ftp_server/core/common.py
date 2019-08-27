12fe9dbc55208c5191e32d53e62030434f5746ee
liliang <18056158203@163.com>
2019-08-27 17:10:14 +0800

    init

---
+++ ftp_server/core/common.py
@@ -0,0 +1,15 @@
+#Author:Zheng Na
+
+import os,json
+import core.settings
+
+def query_user(username):  ####查询用户
+    filelist = os.listdir(core.settings.userinfo_dir)  ####列出指定目录下的所有文件和子目录，包括隐藏文件，并以列表方式打印
+    dict = {}
+    for filename in filelist:
+        with open (os.path.join( core.settings.userinfo_dir,filename),'r',encoding='utf-8') as f:
+            content = json.load(f)  ####json反序列化
+            if content['username'] == username:
+                dict = {'filename':filename,'content':content}
+                # print("查询结果：",dict)
+                return dict
